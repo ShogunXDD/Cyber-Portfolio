8844dccfed37d19d3f00608ace26de39bcbf3f6e
Aditya Yadav <145851307+ShogunXDD@users.noreply.github.com>
2026-05-05 12:32:49 +0530

    Delete projects/blue team directory

--- projects/blue team/stop_scraper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Stopping Scraper backend...
-for /f "tokens=5" %%a in ('netstat -aon ^| find "8501" ^| find "LISTENING"') do taskkill /f /pid %%a >nul 2>nul
-echo Backend stopped successfully!
-pause
